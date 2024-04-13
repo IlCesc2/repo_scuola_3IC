2f810f94d16cb7e2869a978c5cf14116b5d4f69d
Marchetti Francesco Maria <francesco.marchetti@cz.lan>
2024-04-13 09:27:13 +0200

    migliorato custom_exception

--- java/errori/custom_exception/Main.java
+++ java/errori/custom_exception/Main.java
@@ -2,61 +2,64 @@ package custom_exception;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 import java.util.UUID;
 
-
-public class Main {
+public class Main2 {
     static String PATH = "custom_exception\\zamdes.csv";
+    static int MAX_LEN = User.class.getDeclaredFields().length;
 
     public static void main(String[] args) {
+        int i = 0;
+        File file = new File(PATH);
+
+        Scanner fileReader = null;
+        ArrayList<User> users = new ArrayList<User>();
+
         try {
-            reading();
+            fileReader = new Scanner(file);
+
+            while (fileReader.hasNextLine()) {
+                if (i ==0) {
+                    i++;
+                    continue;
+                }
+                User user = reading(file, fileReader.nextLine(), i);
+                users.add(user);
+                i++;
+            }
         } catch (CException e) {
             System.out.println(e.getMessage());
         } catch (FileNotFoundException e) {
             System.out.println("File non trovato");
+        } finally {
+            fileReader.close();
+        }
+        for (User user : users) {
+            System.out.println(user.toString());
         }
     }
 
-    public static void reading() throws CException, FileNotFoundException {
-        File file = new File(PATH);
-        Scanner fileReader = new Scanner(file);
-        int MAX_LEN = User.class.getDeclaredFields().length;
-        System.out.println(MAX_LEN);
-        ArrayList<User> users = new ArrayList<User>();
-        int i = 0;
-
+    public static User reading(File file, String currentLine, int i) throws CException, FileNotFoundException {
         try {
-            while (fileReader.hasNextLine()) {
-                String row = fileReader.nextLine();
 
-                String[] splitRow = row.split(";");
-                if (splitRow.length < MAX_LEN)
-                    throw new CException("Missing parameters");
+            String[] splitRow = currentLine.split(";");
+            if (splitRow.length < MAX_LEN)
+                throw new CException("Missing parameters");
 
-                User user = new User(splitRow[0], splitRow[1], splitRow[2], splitRow[3], splitRow[4], splitRow[5],
-                        splitRow[6], splitRow[7]);
-
-                users.add(user);
-                i++;
-            }
-
+            User user = new User(splitRow[0], splitRow[1], splitRow[2], splitRow[3], splitRow[4], splitRow[5],
+                    splitRow[6], splitRow[7]);
+            return user;
         } catch (CException e) {
             try {
-                String line = Files.readAllLines(Paths.get(PATH)).get(i);
+                ArrayList<String> splitRow = new ArrayList<>(Arrays.asList(currentLine.split(";")));
 
-                ArrayList<String> splitRow = new ArrayList<>(Arrays.asList(line.split(";")));
-
-                String id = genRandId();
+                String id = "Aiuto!";
                 String oneTimePW = genOneTimePW();
 
                 splitRow.add(1, id);
@@ -67,27 +70,20 @@ public class Main {
 
                 lines.set(i, out);
                 Files.write(file.toPath(), lines);
-
+                User user = new User(splitRow.get(0), splitRow.get(1), splitRow.get(2), splitRow.get(3),
+                        splitRow.get(4), splitRow.get(5),
+                        splitRow.get(6), splitRow.get(7));
+                return user;
             } catch (IOException err) {
                 System.out.println("Error when trying to modify csv");
             }
-        } finally {
-            fileReader.close();
         }
-        for (User user : users) {
-            System.out.println(user.toString());
-        }
-    }
-
-    public static String genRandId() {
-        Random rand = new Random();
-        int int_random = rand.nextInt(9000);
-        return int_random + "";
+        return null;
 
     }
 
     public static String genOneTimePW() {
         UUID uuid = UUID.randomUUID();
-        return uuid.toString();
+        return uuid.toString().substring(0, 6);
     }
 }
