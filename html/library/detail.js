194c4406cb33a935193fd852dcdfd96931ff80e6
Marchetti Francesco Maria <francesco.marchetti@cz.lan>
2025-04-09 09:02:27 +0200

    Open Library hp dobe

---
+++ html/library/detail.js
@@ -0,0 +1,5 @@
+function onPageLoads(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+
+}
--- html/library/script.js
+++ html/library/script.js
@@ -1,16 +1,20 @@
 let results = []
 
 async function onInputValueChange(){
+    results=[]
     const value = document.querySelector("input").value
-    console.log(value)
-    await fetch(`https://openlibrary.org/search.json?q=${value}`).then(async (res)=>{
+    document.getElementById("loading").style.display= "inline"
+    const url =`https://openlibrary.org/search.json?title=${value}` // "https://openlibrary.org/search.json?q=bible"    
+    await fetch(url).then(async (res)=>{
         const data = await res.json()
         results= data.docs
         console.log(results)
         refreshResults()
+        document.getElementById("loading").style.display="none"
     })
    
 }
+
 
 function refreshResults(){
 
@@ -20,13 +24,17 @@ function refreshResults(){
     results.forEach((r)=>{
         console.log(r)
         const newBook = document.createElement("div")
+        newBook.classList.add("book")
         const title = document.createElement("h2")
         const img = document.createElement("img")
         title.textContent=r.title
         img.src=`https://covers.openlibrary.org/b/id/${r.cover_i}.jpg`
+        img.classList.add("cover")
         newBook.appendChild(title)
         newBook.appendChild(img)
         res.appendChild(newBook)
     })
 }
 document.querySelector("input").addEventListener("input",(e)=> onInputValueChange())
+
+//onInputValueChange()
