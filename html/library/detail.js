35c41191a93fbfd946f28a4b0ad81f90918c439a
Marchetti Francesco Maria <francesco.marchetti@cz.lan>
2025-04-28 10:45:12 +0200

    asdas

--- html/library/detail.js
+++ html/library/detail.js
@@ -3,3 +3,13 @@ function onPageLoads(){
     const id = urlParams.get('id');
 
 }
+
+async function fetchByID(id){
+    const url =`https://openlibrary.org/search.json?cover_i=${id}` // "https://openlibrary.org/search.json?q=bible"    
+    await fetch(url).then(async (res)=>{
+        const data = await res.json()
+        results= data.docs
+        console.log(results)
+        document.getElementById("loading").style.display="none"
+    })
+}
